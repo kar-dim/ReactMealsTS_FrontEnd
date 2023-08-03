859d19a48249de3778d6df9a233c7ffa89a33073
Mimis <dim_kar@outlook.com>
2023-08-03 21:34:00 +0300

    More Auth0 stuff (basic user data, idToken claims)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import CartButton from './CartButton';
 import HeaderStyles from './Header.module.css';
 import {useCartContext} from '../contexts/cart-context';
@@ -8,11 +8,35 @@ import { Link } from "react-router-dom";
 import imgBg from '../media/bg.webp';
 import Auth0SignInOffButton from './Auth0SignInOffButton';
 import { useAuth0 } from "@auth0/auth0-react";
+import Settings from '../other/PublicSettings';
 
 //renders the top header bar
 const Header = ()  => {
 
-    const {isAuthenticated} = useAuth0();
+    const {getIdTokenClaims, isAuthenticated} = useAuth0();
+    const [userLoggedIn, setUserLoggedIn] = useState<IAuth0User | undefined>(undefined);
+
+    //get the IdToken Claims, which contain the user's metadata (user NAME/LASTNAME/ADDRESS), these are added
+    //in the idToken in Auth0 dashboard -> onPostLogin AUTH0 ACTION
+    useEffect(() => {
+        if (isAuthenticated) {
+            const getIdClaimsAsync = async() => {
+                let claims = await getIdTokenClaims();
+                if (claims != undefined) {
+                    setUserLoggedIn({
+                        name: claims[`${Settings.auth0_audience}/user_metadata.name`],
+                        last_name: claims[`${Settings.auth0_audience}/user_metadata.last_name`],
+                        email: claims[`email`] ?? "",
+                        address: claims[`${Settings.auth0_audience}/user_metadata.address`],
+                    });
+                }
+            }
+            getIdClaimsAsync();
+        } else
+            setUserLoggedIn(undefined);
+
+        }, [isAuthenticated]);
+
     const [showModal, setShowModal] = useState<boolean>(false);
     const {cartItems} = useCartContext();
 
@@ -41,6 +65,7 @@ const Header = ()  => {
                     <Link to="/" id={HeaderStyles.header_home}>Jimmys Foodzilla</Link>
                     <button className={HeaderStyles.custom_button}><Link id={HeaderStyles.header_about_link} to="/about">About</Link></button>
                     <Auth0SignInOffButton text={isAuthenticated ? "Logout" : "Login"} isLogin = {!isAuthenticated} />
+                    {(isAuthenticated && userLoggedIn !== undefined && userLoggedIn.name != undefined) && <span id={HeaderStyles.header_user_name}>Welcome back, {userLoggedIn.name}!</span>}
                 </div>
 
                 <CartButton cartItemsCounter = {cartItems.length} cartButtonClick={clickCartHandler} />
