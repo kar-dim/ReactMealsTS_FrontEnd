cd65ea5281fe35e4498fd501b7d167a71b26fb8e
Mimis <dim_kar@outlook.com>
2023-08-11 19:06:45 +0300

    vercel test

--- src/other/PublicSettings.ts
+++ src/other/PublicSettings.ts
@@ -2,9 +2,10 @@ const Settings = {
     auth0_domain : "dev-f0vakdckhtwh0dl8.us.auth0.com",
     auth0_clientId: "lUYQ4bVv26qAc2y7fK1jw9zXenl9sw5a",
     auth0_audience: "https://jimmys/api",
-    frontend_url: "http://localhost:3000",
+    frontend_url: import.meta.env.VITE_FRONTEND_URL,
     //backend_url: "http://localhost:5179",
     backend_url: "https://solely-useful-grouse.ngrok-free.app"
 };
+console.log("FRONTEND_URL",Settings.frontend_url);
 
 export default Settings;
