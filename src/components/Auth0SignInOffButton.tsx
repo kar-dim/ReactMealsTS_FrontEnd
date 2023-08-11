947f119a85cf090589d211dff7526cbc64177ecb
Mimis <dim_kar@outlook.com>
2023-08-11 19:17:33 +0300

    vercel test 2.1

--- src/components/Auth0SignInOffButton.tsx
+++ src/components/Auth0SignInOffButton.tsx
@@ -5,7 +5,7 @@ const Auth0SignInOffButton = (props: {text: string; isLogin: boolean})=> {
   const { loginWithRedirect, logout } = useAuth0();
   return (
     <button className={HeaderStyles.custom_button} onClick=
-    {() => props.isLogin ? loginWithRedirect() : logout({ logoutParams: { returnTo: "http://localhost:3000/" } })}>
+    {() => props.isLogin ? loginWithRedirect() : logout({ logoutParams: { returnTo: import.meta.env.VITE_FRONTEND_URL } })}>
       {props.text}
     </button>
   );
