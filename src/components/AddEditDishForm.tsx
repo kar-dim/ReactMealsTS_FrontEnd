c3168a98fb864387cc30bef20146844d3c89381b
Mimis <dim_kar@outlook.com>
2023-09-30 10:38:07 +0300

    misc stuff

--- src/components/AddEditDishForm.tsx
+++ src/components/AddEditDishForm.tsx
@@ -15,21 +15,22 @@ interface IAddEditDishFormProps {
 }
 
 const AddEditDishForm = ({preFilledValues, addOrEditDish,addOrEditDishImageHandler, addOrEditDishButton, isUsedForAdd} : IAddEditDishFormProps) => {
+    const spacing : string = isUsedForAdd ? " " : "";
     return (
         <div>
             <div>
                 <form id={adminStyle.add_dish_form} onSubmit={(event) => addOrEditDish(event)}>
                     <div id={adminStyle.add_dish_1}>
                         <div id={adminStyle.add_dish_1_name}>
-                            <label id={adminStyle.adminmenu_dish_name_label} htmlFor={adminStyle.adminmenu_dish_name}>Dish name:</label>
+                            <label id={adminStyle.adminmenu_dish_name_label} htmlFor={adminStyle.adminmenu_dish_name}>Dish name:</label><span>{spacing}</span>
                             <input required type="text" id={adminStyle.adminmenu_dish_name} defaultValue={preFilledValues == null ? "" : preFilledValues.dish_name} name="dish_name" maxLength={50}></input>
                         </div>
                         <div id={adminStyle.add_dish_1_description}>
-                            <label id={adminStyle.adminmenu_dish_description_label} htmlFor={adminStyle.adminmenu_dish_description}>Dish Description:</label>
+                            <label id={adminStyle.adminmenu_dish_description_label} htmlFor={adminStyle.adminmenu_dish_description}>Dish Description:</label><span>{spacing}</span>
                             <input required type="text" id={adminStyle.adminmenu_dish_description} defaultValue={preFilledValues == null ? "" : preFilledValues.dish_description} name="dish_description" maxLength={70}></input>
                         </div>
                         <div id={adminStyle.add_dish_1_price}>
-                            <label id={adminStyle.adminmenu_dish_price_label} htmlFor={adminStyle.adminmenu_dish_price}>Dish Price:</label>
+                            <label id={adminStyle.adminmenu_dish_price_label} htmlFor={adminStyle.adminmenu_dish_price}>Dish Price:</label><span>{spacing}</span>
                             <input required type="number" id={adminStyle.adminmenu_dish_price} defaultValue={preFilledValues == null ? 0.01 : preFilledValues.price} placeholder="0.00" name="dish_price" min="0" step="0.01" pattern="^\d+\.\d{2}$"></input>
                         </div>
                     </div>
